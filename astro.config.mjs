6ebda07df6b01d28c0d6aaeae6ec6fda69254615
Nick Winans <nick@winans.codes>
2023-06-09 12:08:06 -0500

    Fix aria issue with heading

--- astro.config.mjs
+++ astro.config.mjs
@@ -26,6 +26,7 @@ export default defineConfig({
           s('path', {fill: 'currentColor', opacity: 0.4, d: 'M579.8 63.2a144.5 144.5 0 0 1 0 204.5L467.5 380a144.6 144.6 0 0 1-219.9-186.3l1.1-1.6c9.4-14.3 29.4-17.7 44.6-7.4 14.4 9.4 17.8 29.4 7.5 44.6l-1.1 1.6a80.7 80.7 0 0 0 122.6 103.9l112.2-112.3A80.6 80.6 0 0 0 430.7 99.9l-1.6 1.1a32.7 32.7 0 0 1-44.6-7.4 32 32 0 0 1 7.4-44.7l1.6-1a144.6 144.6 0 0 1 186.3 15.3z'})
         ])
       ]),
+      properties: {ariaHidden: true, tabIndex: -1},
       group: h('section.heading-group')
   }]],
     remarkPlugins: [remarkReadingTime]
