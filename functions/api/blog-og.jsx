e545521c91c55db57f426a779214680c24db5a48
Nick Winans <nick@winans.codes>
2023-11-24 13:42:47 -0800

    Try generating OG images with cloudflare

---
+++ functions/api/blog-og.jsx
@@ -0,0 +1,117 @@
+import React from "react";
+import { ImageResponse } from "@cloudflare/pages-plugin-vercel-og/api";
+import LexendBlack from "../../public/fonts/LexendDeca-Black.ttf";
+import Lexend from "../../public/fonts/LexendDeca-ExtraBold.ttf";
+import Geologica from "../../public/fonts/Geologica-Regular.ttf";
+
+export const onRequest = async ({request}) => {
+  const searchParams = new URLSearchParams(new URL(request.url).search);
+  const title = searchParams.get("title") || "use ?title=";
+  const description = searchParams.get("description") || "use ?description=";
+  const date = searchParams.get("date") || "use ?date=";
+  const minutes = searchParams.get("minutes") || "use ?minutes=";
+
+  return new ImageResponse(
+    (
+      <div
+        style={{
+          backgroundImage:
+            "radial-gradient(ellipse at 50% 0px, #dcfce7, #dbeafe, #f3e8ff)",
+          height: "100%",
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "space-between",
+          width: "100%",
+          padding: "55px 65px",
+        }}
+      >
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+          }}
+        >
+          <div
+            style={{
+              fontSize: 64,
+              fontFamily: "Lexend",
+              lineHeight: "1.1em",
+              paddingBottom: "20px",
+            }}
+          >
+            {title}
+          </div>
+          <div
+            style={{
+              fontSize: 48,
+              fontFamily: "Geologica",
+              lineHeight: "1.15em",
+            }}
+          >
+            {description}
+          </div>
+        </div>
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "space-between",
+            alignItems: "flex-end",
+          }}
+        >
+          <div
+            style={{
+              fontSize: 36,
+              display: "flex",
+              alignItems: "center",
+              fontFamily: "Geologica",
+            }}
+          >
+            <img
+              src="https://github.com/Nicell.png"
+              height="150px"
+              width="150px"
+              style={{ borderRadius: "75px", marginRight: "24px" }}
+            />
+            <span>
+              {new Date(date).toLocaleDateString("en-us", {
+                year: "numeric",
+                month: "short",
+                day: "numeric",
+              })}{" "}
+              | {minutes} min read
+            </span>
+          </div>
+          <div
+            style={{
+              fontSize: 48,
+              fontFamily: "LexendBlack",
+            }}
+          >
+            nick.winans.io
+          </div>
+        </div>
+      </div>
+    ),
+    {
+      width: 1200,
+      height: 630,
+      fonts: [
+        {
+          name: "Lexend",
+          data: Lexend,
+          style: "normal",
+        },
+        {
+          name: "LexendBlack",
+          data: LexendBlack,
+          style: "normal",
+        },
+        {
+          name: "Geologica",
+          data: Geologica,
+          style: "normal",
+        },
+      ],
+    }
+  );
+}
