39ff534661cb550ac743352793105b5207eddb80
Nick Winans <nick@winans.codes>
2024-04-22 22:38:41 +0000

    Add table of contents

--- astro.config.mjs
+++ astro.config.mjs
@@ -12,20 +12,21 @@ import {
   transformerMetaWordHighlight
 } from '@shikijs/transformers'
 import { addCopyButton } from 'shiki-transformer-copy-button'
+import sectionize from '@hbsnow/rehype-sectionize';
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://nick.winans.io",
   integrations: [tailwind(), sitemap(), robotsTxt(), mdx({
-    rehypePlugins: [rehypeSlug, [rehypeAutolinkHeadings, {
+    rehypePlugins: [rehypeSlug, sectionize, [rehypeAutolinkHeadings, {
       behavior: 'before',
       content: h('span.icon-link', [
-        s('svg', {ariaHidden: true, width: 32, height: 32, xmlns: 'http://www.w3.org/2000/svg', viewBox: '0 0 448 512'}, [
+        s('svg', {ariaHidden: true, xmlns: 'http://www.w3.org/2000/svg', viewBox: '0 0 448 512'}, [
           s('path', {fill: 'currentColor', d: 'M181.3 32.4c17.4 2.9 29.2 19.4 26.3 36.8L197.8 128h95.1l11.5-69.3c2.9-17.4 19.4-29.2 36.8-26.3s29.2 19.4 26.3 36.8L357.8 128H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H347.1L325.8 320H384c17.7 0 32 14.3 32 32s-14.3 32-32 32H315.1l-11.5 69.3c-2.9 17.4-19.4 29.2-36.8 26.3s-29.2-19.4-26.3-36.8l9.8-58.7H155.1l-11.5 69.3c-2.9 17.4-19.4 29.2-36.8 26.3s-29.2-19.4-26.3-36.8L90.2 384H32c-17.7 0-32-14.3-32-32s14.3-32 32-32h68.9l21.3-128H64c-17.7 0-32-14.3-32-32s14.3-32 32-32h68.9l11.5-69.3c2.9-17.4 19.4-29.2 36.8-26.3zM187.1 192L165.8 320h95.1l21.3-128H187.1z'})
         ])
       ]),
       properties: {ariaHidden: true, tabIndex: -1},
-      group: h('section.heading-group')
+      group: h('span.heading-group')
   }]],
     remarkPlugins: [remarkReadingTime]
   })],
