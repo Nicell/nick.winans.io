56e12f385697e3cb54ca78d42d7bb75f6e220ba6
Nick Winans <nick@winans.io>
2024-04-24 01:32:35 +0200

    fix bad prop

--- shiki-transformer-copy-button.mjs
+++ shiki-transformer-copy-button.mjs
@@ -13,7 +13,7 @@ export function addCopyButton() {
           onclick: `
           navigator.clipboard.writeText(this.dataset.code);
           this.classList.add('copied');
-          setTimeout(() => this.classList.remove('copied'), ${toggleMs})
+          setTimeout(() => this.classList.remove('copied'), 3000)
         `,
         },
         [h("span", { class: "ready" }), h("span", { class: "success" })]
