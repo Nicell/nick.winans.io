023861be4a8a8a5994a78ff2ecd0db6eaaa6d41b
Nick Winans <nick.win999@gmail.com>
2025-01-26 18:35:38 -0800

    sitemap, robots

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 // @ts-check
 import { defineConfig } from "astro/config";
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://nick.winans.io",
+  integrations: [sitemap()],
   image: {
     remotePatterns: [{ protocol: "https" }],
   },
